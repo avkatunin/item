6d392ebd3a90c9e11f8a0df3849a111e76976187
andrei <katunin4@yandex.ru>
2020-01-24 16:30:59 +0300

    in-memory

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS ITEM;
+
+create table ITEM
+(
+    id     int primary key,
+    attr1  int,
+    attr2  int,
+    attr3  int,
+    attr4  int,
+    attr5  varchar(10),
+    attr6  varchar(10),
+    attr7  varchar(10),
+    attr8  varchar(10),
+    attr9  varchar(10),
+    attr10 varchar(10)
+);
